896848b3102f75e4d08a85de63607c8c7fa4dcaa
Yusuf Mustafa <yusufmustafa1@outlook.com>
2023-12-20 17:51:28 +0200

    Add Employee Controller , Index action, Index view

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using MvcPureHtml.Models;
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcPureHtml.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        CompanyModel db;
+
+        public EmployeeController()
+        {
+            db = new CompanyModel();
+        }
+        // GET: Employee
+        public ActionResult Index()
+        {
+            var emps = db.Employees.Include(dept => dept.Department).ToList();
+            return View(emps);
+        }
+    }
+}
